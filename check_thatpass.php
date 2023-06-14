2b864b31d32954ee72c0d950890326369f9dc329
David St John <davestj@gmail.com>
2023-06-14 07:43:35 -0700

    Update check_thatpass.php

--- check_thatpass.php
+++ check_thatpass.php
@@ -124,19 +124,6 @@ public function decrypt_thatpass($key,$password){
 }
 
 
-//EXAMPLE CASE USAGE
-$_pass  = "g#Ti678Kd!l0D";
-$passobj = new ThePassMan();
-$mypass = $passobj->check_thatpass($_pass);
-   
-echo 'Your password of '.$_pass.' is '.$mypass.'  <br>';
-$hashit = $passobj->gen_thathash($_pass);
-echo "Hashed password result: $hashit <br>";
 
-$crypted = $passobj->crypt_thatpass($_pass,$hashit);
-echo "Crypted hash with password as key: $crypted<br>";
-
-$decrypted = $passobj->decrypt_thatpass($_pass,$crypted);
-echo "decrypted hash: $decrypted<br>";
 
 ?>
